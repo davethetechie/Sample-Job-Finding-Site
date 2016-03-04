2cbeccf4ac7ce6a1af2470b96e673200cf168a03
David Sirota <daviddjs@m.com>
2016-03-04 02:38:48 +0000

    added categories for job posts and edits

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -4,9 +4,14 @@ class JobsController < ApplicationController
     before_action :find_job, only: [:show, :edit, :update, :destroy]
     
     def index
-        @jobs = Job.all.order("created_at DESC")
-        
+        if params[:category].blank?
+            @jobs = Job.all.order("created_at DESC")
+        else
+            @category_id = Category.find_by(name: params[:category]).id
+            @jobs = Job.where(category_id: @category_id).order("created_at DESC")
+        end
     end
+    
     
     def show
         
@@ -48,7 +53,7 @@ class JobsController < ApplicationController
     private
     
     def jobs_params
-        params.require(:job).permit(:title, :description, :name, :url, :avatar)
+        params.require(:job).permit(:title, :description, :name, :url, :avatar, :category_id)
     end
     
     def find_job
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160301152207) do
+ActiveRecord::Schema.define(version: 20160303225919) do
+
+  create_table "categories", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -47,6 +53,7 @@ ActiveRecord::Schema.define(version: 20160301152207) do
     t.datetime "avatar_updated_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "category_id"
   end
 
   create_table "plans", force: true do |t|
